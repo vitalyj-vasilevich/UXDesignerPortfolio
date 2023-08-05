1db8ffc71c002c754b75cedac72d731f8e4c1a17
Alexander Koktev <a.koktev@syberry.com>
2023-08-05 21:03:44 +0300

    Split home page

--- portfolio-app/src/Pages/Home/Home.jsx
+++ portfolio-app/src/Pages/Home/Home.jsx
@@ -1,11 +1,27 @@
-import './Home.scss';
-import MainTitle from './MainTitle/MainTitle';
+import "./Home.scss";
+import { createTheme } from "@mui/material/styles";
+import MainTitle from "./MainTitle/MainTitle";
+import PageWrapper from "../../components/PageWrapper/PageWrapper";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 
 export function Home() {
   return (
-    <main className='main-page-continer'>
-      <MainTitle />
-    </main>
+    <PageWrapper theme={lightTheme}>
+      <main>
+        <MainTitle />
+      </main>
+    </PageWrapper>
   );
 }
 
